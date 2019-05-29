9c20861aa211b1a85e282601880e2c5f3fc7706c
Ravikiran <Ravikiran@192.168.2.11>
2019-05-29 08:56:08 +0530

    start1.o

---
+++ src/main/java/kiran/com/rest/base/TestRunner.java
@@ -0,0 +1,16 @@
+package kiran.com.rest.base;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(glue= {},
+				tags= {"@Test"},
+				format= {"pretty","html:Test-OutPut"},
+				dryRun=true)
+
+public class TestRunner {
+
+}
---
+++ src/main/kiran/com/rest/base/cucumber.feature
@@ -0,0 +1,8 @@
+Feature: Test Hello
+
+
+Background:
+Given Runn
+
+@Test
+Scenario: runn success
